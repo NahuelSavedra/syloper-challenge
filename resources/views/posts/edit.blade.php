c1378f38677704a822d3609b69220ff7a7cf819c
Nahuel Savedra <nsavedra97@gmail.com>
2021-08-05 20:30:45 -0300

    Fixed edit view

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 
 
-    <section>
+    <section class="container">
         <div>
             <header>
-                <h2>Editar el post</h2>
+                <h2 class="text-center mt-4">Editar post</h2>
             </header>
         </div>
     </section>
@@ -17,37 +17,32 @@
         <form method="POST" action="{{route('posts.update',$post)}}" enctype="multipart/form-data">
             @csrf
             @method('PUT')
-            <label for="titulo">Title: </label>
-            <input type="text" name="titulo"
-                   @error('titulo') class="danger" @enderror
-                   value="{{ $post->titulo }}">
-
+            <div class="form-group">
+                <label for="titulo" class="form-label mt-4">Titulo</label>
+                <input type="text" class="form-control" id="titulo" name="titulo"
+                       value="{{ $post->titulo }}">
+            </div>
             @error('titulo')
-            <p class="error">{{ $errors->first('titulo') }}</p>
+            <p class="text-danger">{{ $errors->first('titulo') }}</p>
             @enderror
 
-            <br>
-            <label for="slug">Slug: </label>
-            <input type="text" name="slug"
-                   @error('slug') class="danger" @enderror
-                   value="{{ $post->slug }}">
+            <div class="form-group">
+                <fieldset>
+                    <label class="form-label mt-4" for="slug">Slug</label>
+                    <input class="form-control" id="slug" name="slug" type="text"
+                           value="{{ $post->slug }}">
+                </fieldset>
+            </div>
 
-            @error('slug')
-            <p class="error">{{ $errors->first('slug') }}</p>
-            @enderror
-
-            <br>
-
-            <label for="descripcion">Descripcion:</label>
-            <textarea @error('descripcion') class="danger" @enderror name="descripcion" id="descripcion" cols="30" rows="10">
-            {{ $post->descripcion }}
-            </textarea>
+            <div class="form-group">
+                <label for="descripcion" class="form-label mt-4">Ingrese su texto</label>
+                <textarea class="form-control" id="descripcion" name="descripcion" rows="5">{{$post->descripcion}}</textarea>
+            </div>
             @error('descripcion')
-            <p class="error">{{ $errors->first('descripcion') }}</p>
+            <p class="text-danger">{{ $errors->first('descripcion') }}</p>
             @enderror
-            <br>
 
-            <button class="btn btn-block btn-primary" type="submit">Enviar</button>
+            <button class="btn btn-primary mt-4" type="submit">Enviar</button>
         </form>
     </div>
 
